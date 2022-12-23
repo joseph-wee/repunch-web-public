0e0aa06eb53484e46081fe5e82b305f7aa429d88
joseph-wee <starlightkor@gmail.com>
2022-12-23 11:16:00 +0900

    design: 레이아웃, 배경색만 구현

---
+++ components/FooterBar.tsx
@@ -0,0 +1,22 @@
+/* ------------- 푸터바 ------------- */
+
+import React from "react";
+import styled from "styled-components";
+
+const FooterBar = () => {
+  return (
+    <>
+      <Container>
+
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.footer`
+  width: 100%;
+  height: 114.35px;
+  background-color: #0A4459;
+`;
+
+export default FooterBar;
