b68cb133ffcb282760e4df7a1806ef65854295f0
joseph-wee <starlightkor@gmail.com>
2022-12-26 16:39:36 +0900

    add: 파일(체크박스 체크표시 이미지) 추가

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,4 @@
 export { default as button_menu } from "./button_menu.svg";
 export { default as arrow_down } from "./arrow_down.svg";
 export { default as arrow_up } from "./arrow_up.svg";
+export { default as ic_check_wht } from "./ic_check_wht.svg";
