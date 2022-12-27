335d2da482837a3911ad084644007192baf39361
joseph-wee <starlightkor@gmail.com>
2022-12-27 17:55:46 +0900

    design&feat: 헤더바 구현 완료(현재 피그마 기준)

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -1,24 +1,48 @@
 /* ------------- 헤더바 ------------- */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
-import { button_menu } from "../assets";
+import { button_menu, ic_cart_wht, ic_close_wht, ic_favorite_wht } from "../assets";
+import Link from "next/link";
+import NavTopBar from "./NavTopBar";
+import NavMobileBar from "./NavMobileBar";
 
-const HeaderBar = () => {
-
+const useHeaderBar = () => {
+  const [isActive, setIsActive] = useState<boolean>(false)
 
 
   return (
     <>
       <Container>
+      <Link href="/" style={{ textDecoration: "none" }}>
         <Title>Repunch</Title>
-        {/* <Image 
-          src={button_menu}
+        </Link>
+        <Menu onClick={() => setIsActive(!isActive)}>
+        <Image 
+          src={isActive ? ic_close_wht : button_menu}
           alt="button_menu"
-          
-        /> */}
+        />
+        </Menu>
+        <Menu>
+        <Link href="/cart" style={{ textDecoration: "none" }}>
+        <Image 
+          src={ic_cart_wht}
+          alt="cart_menu_button"
+        />
+        </Link>
+        </Menu>
+        <Menu>
+        <Link href="/favorite" style={{ textDecoration: "none" }}>
+        <Image 
+          src={ic_favorite_wht}
+          alt="favorite_menu_button"
+        />
+        </Link>
+        </Menu>
       </Container>
+      <NavTopBar />
+      <NavMobileBar isActive={isActive} setIsActive={setIsActive}/>
     </>
   );
 };
@@ -37,11 +61,32 @@ const Container = styled.header`
   background-color: #ff5c01;
 `;
 const Title = styled.h1`
-  font-weight: 900;
-  font-size: 30px;
-  line-height: 130%;
-  letter-spacing: -0.02em;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 900;
+font-size: 30px;
+line-height: 130%;
+/* identical to box height, or 39px */
+
+letter-spacing: -0.02em;
   color: #ffffff;
 `;
+const Menu = styled.div` 
+  position: absolute; 
+  right: 20px;
+  &:nth-of-type(2) {
+    right: 64px;
+    @media screen and (max-width: 767px) {
+      display: none;
+  };
+  }
+  &:nth-of-type(3) {
+    right: 108px;
+    @media screen and (max-width: 767px) {
+      display: none;
+  };
+  }
+  cursor: pointer;
+`
 
-export default HeaderBar;
+export default useHeaderBar;
