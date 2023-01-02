47cad3cf893b901949d5e35df9788acc14403d69
joseph-wee <starlightkor@gmail.com>
2023-01-02 20:13:00 +0900

    refactor: 코드 리펙토링 툴로 리펙토링

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -3,46 +3,44 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
-import { button_menu, ic_cart_wht, ic_close_wht, ic_favorite_wht } from "../assets";
+import {
+  button_menu,
+  ic_cart_wht,
+  ic_close_wht,
+  ic_favorite_wht,
+} from "../assets";
 import Link from "next/link";
 import NavTopBar from "./NavTopBar";
 import NavMobileBar from "./NavMobileBar";
 
 const useHeaderBar = () => {
-  const [isActive, setIsActive] = useState<boolean>(false)
-
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   return (
     <>
       <Container>
-      <Link href="/" style={{ textDecoration: "none" }}>
-        <Title>Repunch</Title>
+        <Link href="/" style={{ textDecoration: "none" }}>
+          <Title>Repunch</Title>
         </Link>
         <Menu onClick={() => setIsActive(!isActive)}>
-        <Image 
-          src={isActive ? ic_close_wht : button_menu}
-          alt="button_menu"
-        />
+          <Image
+            src={isActive ? ic_close_wht : button_menu}
+            alt="button_menu"
+          />
         </Menu>
         <Menu>
-        <Link href="/cart" style={{ textDecoration: "none" }}>
-        <Image 
-          src={ic_cart_wht}
-          alt="cart_menu_button"
-        />
-        </Link>
+          <Link href="/cart" style={{ textDecoration: "none" }}>
+            <Image src={ic_cart_wht} alt="cart_menu_button" />
+          </Link>
         </Menu>
         <Menu>
-        <Link href="/favorite" style={{ textDecoration: "none" }}>
-        <Image 
-          src={ic_favorite_wht}
-          alt="favorite_menu_button"
-        />
-        </Link>
+          <Link href="/favorite" style={{ textDecoration: "none" }}>
+            <Image src={ic_favorite_wht} alt="favorite_menu_button" />
+          </Link>
         </Menu>
       </Container>
       <NavTopBar />
-      <NavMobileBar isActive={isActive} setIsActive={setIsActive}/>
+      <NavMobileBar isActive={isActive} setIsActive={setIsActive} />
     </>
   );
 };
@@ -61,32 +59,32 @@ const Container = styled.header`
   background-color: #ff5c01;
 `;
 const Title = styled.h1`
-font-family: 'Roboto';
-font-style: normal;
-font-weight: 900;
-font-size: 30px;
-line-height: 130%;
-/* identical to box height, or 39px */
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 30px;
+  line-height: 130%;
+  /* identical to box height, or 39px */
 
-letter-spacing: -0.02em;
+  letter-spacing: -0.02em;
   color: #ffffff;
 `;
-const Menu = styled.div` 
-  position: absolute; 
+const Menu = styled.div`
+  position: absolute;
   right: 20px;
   &:nth-of-type(2) {
     right: 64px;
     @media screen and (max-width: 767px) {
       display: none;
-  };
+    }
   }
   &:nth-of-type(3) {
     right: 108px;
     @media screen and (max-width: 767px) {
       display: none;
-  };
+    }
   }
   cursor: pointer;
-`
+`;
 
 export default useHeaderBar;
