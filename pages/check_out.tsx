d915837b049d96a239745da7b793f759ddf1d15d
joseph-wee <starlightkor@gmail.com>
2023-01-24 13:17:40 +0900

    design: 즐겨찾기 페이지 마크업

---
+++ pages/check_out.tsx
@@ -0,0 +1,458 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { ic_check_wht, paypal } from "../assets";
+import { CheckOutMeterageProduct } from "../components";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const useCheck_out = () => {
+  const [deliveryIsChecked, setDeliveryIsChecked] = useState<boolean>(true);
+  const [paymentIsChecked, setPaymentIsChecked] = useState<boolean>(true);
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  return (
+    <>
+      <Container>
+        <Title>Check out</Title>
+        <ContentTitle>Product</ContentTitle>
+        <CheckOutMeterageProduct />
+        <CheckOutMeterageProduct />
+        <ContentTitle>Order Profile</ContentTitle>
+        <ContentWrapper>
+          <EditButton>Edit</EditButton>
+          <ProfileCorperationName>Repp.corp</ProfileCorperationName>
+          <ProfileName>JKim</ProfileName>
+          <ProfilePhoneNumber>+82(0)10-8628-1024</ProfilePhoneNumber>
+        </ContentWrapper>
+        <ContentTitle>Shipping Address</ContentTitle>
+        <ContentWrapper>
+          <EditButton>Edit</EditButton>
+          <AddressTitle>My1</AddressTitle>
+          <AddressText>#809</AddressText>
+          <AddressText>#809, 8dong ssangyoung</AddressText>
+          <AddressText>daechi dong, gangnamgu</AddressText>
+          <AddressText>korea</AddressText>
+          <AddressText>06285</AddressText>
+          <AddressPhoneNumber>821086281024</AddressPhoneNumber>
+          <AddressButton>+ Add a new address</AddressButton>
+          <AddressButton>Select other address</AddressButton>
+        </ContentWrapper>
+        <ContentTitle>Delivery</ContentTitle>
+        <ContentWrapper>
+          <RadioButton type="radio" id="ship" name="delivery" />
+          <RadioLabel
+            htmlFor="ship"
+            isChecked={deliveryIsChecked}
+            onClick={() => setDeliveryIsChecked(true)}
+          >
+            <DefaultCircle isChecked={deliveryIsChecked} />
+            <CheckedCircle isChecked={deliveryIsChecked}>
+              <SmallCircle />
+            </CheckedCircle>
+            By ship ($1.300)
+          </RadioLabel>
+          <DashLine />
+          <RadioButton type="radio" id="air" name="delivery" />
+          <RadioLabel
+            htmlFor="air"
+            isChecked={!deliveryIsChecked}
+            onClick={() => setDeliveryIsChecked(false)}
+          >
+            <DefaultCircle isChecked={!deliveryIsChecked} />
+            <CheckedCircle isChecked={!deliveryIsChecked}>
+              <SmallCircle />
+            </CheckedCircle>
+            By air ($1.300)
+          </RadioLabel>
+        </ContentWrapper>
+        <ContentTitle>Payment</ContentTitle>
+        <ContentWrapper>
+          <RadioButton type="radio" id="ship" name="delivery" />
+          <RadioLabel
+            htmlFor="ship"
+            isChecked={deliveryIsChecked}
+            onClick={() => setPaymentIsChecked(true)}
+          >
+            <DefaultCircle isChecked={paymentIsChecked} />
+            <CheckedCircle isChecked={paymentIsChecked}>
+              <SmallCircle />
+            </CheckedCircle>
+            Credit Card
+          </RadioLabel>
+          <DashLine />
+          <RadioButton type="radio" id="air" name="delivery" />
+          <RadioLabel
+            htmlFor="air"
+            isChecked={!deliveryIsChecked}
+            onClick={() => setPaymentIsChecked(false)}
+          >
+            <DefaultCircle isChecked={!paymentIsChecked} />
+            <CheckedCircle isChecked={!paymentIsChecked}>
+              <SmallCircle />
+            </CheckedCircle>
+            <Image src={paypal} alt={"paypal_image"} width={63} height={21} />
+          </RadioLabel>
+        </ContentWrapper>
+        <ContentTitle>Billng Address</ContentTitle>
+        <ContentWrapper>
+          <CheckBox type="checkbox" id="address" />
+          <CheckBoxLabel
+            htmlFor="address"
+            isChecked={isChecked}
+            onClick={() => setIsChecked(!isChecked)}
+          >
+            <Box isChecked={isChecked} img={ic_check_wht.src} />
+            Set as a shipping address
+          </CheckBoxLabel>
+          <BillingAddressWrapper isChecked={isChecked}>
+            <EditButton>Edit</EditButton>
+            <AddressTitle>My1</AddressTitle>
+            <AddressText>#809</AddressText>
+            <AddressText>#809, 8dong ssangyoung</AddressText>
+            <AddressText>daechi dong, gangnamgu</AddressText>
+            <AddressText>korea</AddressText>
+            <AddressText>06285</AddressText>
+            <AddressPhoneNumber>821086281024</AddressPhoneNumber>
+            <AddressButton>+ Add a new address</AddressButton>
+            <AddressButton>Select other address</AddressButton>
+          </BillingAddressWrapper>
+        </ContentWrapper>
+        <PriceWrapper>
+          <FlexWrapper>
+            <PriceTitle>Item subtotal</PriceTitle>
+            <Price>$32.25</Price>
+          </FlexWrapper>
+          <FlexWrapper>
+            <PriceTitle>
+              Delivery by ship
+              <QuestionMark>?</QuestionMark>
+            </PriceTitle>
+            <Price>Free</Price>
+          </FlexWrapper>
+          <FlexWrapper>
+            <PriceTitle>
+              Tax <QuestionMark>?</QuestionMark>
+            </PriceTitle>
+            <Price>$32.25</Price>
+          </FlexWrapper>
+          <Line />
+          <FlexWrapper>
+            <TotalTitle>Total</TotalTitle>
+            <TotalPrice>$62.25</TotalPrice>
+          </FlexWrapper>
+        </PriceWrapper>
+      </Container>
+      <Line />
+      <ButtonWrapper>
+        <CancelButton onClick={() => router.push("/cart")}>Cancel</CancelButton>
+        <CheckoutButton onClick={() => router.push("/payment_complete")}>
+          Checkout
+        </CheckoutButton>
+      </ButtonWrapper>
+    </>
+  );
+};
+
+const Container = styled.div`
+  margin: 0 auto;
+  margin-top: 16px;
+  padding-top: 20px;
+  max-width: 427px;
+  @media screen and (max-width: 767px) {
+    padding-left: 20px;
+    padding-right: 20px;
+    max-width: 100%;
+  }
+`;
+const Title = styled.div`
+  margin-bottom: 20px;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: -0.011em;
+  color: #0a4459;
+  @media screen and (max-width: 767px) {
+    font-size: 22px;
+    line-height: 26px;
+  }
+`;
+const ContentTitle = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  padding-left: 16px;
+  height: 30px;
+  background: #f2f6f8;
+  border: 0.79402px solid #dee8ec;
+  border-radius: 2px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
+  color: #0a4459;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  margin-bottom: 16px;
+  padding: 16px;
+  border: 1px solid #dee8ec;
+  border-radius: 2px;
+`;
+const EditButton = styled.button`
+  position: absolute;
+  right: 16px;
+  border: none;
+  background-color: #ffffff;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
+  color: #a4abba;
+  cursor: pointer;
+`;
+const ProfileCorperationName = styled.div`
+  margin-bottom: 4px;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+  color: #0a4459;
+`;
+const ProfileName = styled.div`
+  margin-bottom: 8px;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+  color: #0a4459;
+`;
+const ProfilePhoneNumber = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  color: #0a4459;
+`;
+const BillingAddressWrapper = styled.div<{ isChecked: boolean }>`
+  display: ${(props) => {
+    return props.isChecked == true ? "none" : "block";
+  }};
+`;
+const AddressTitle = styled.div`
+  margin-bottom: 12px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  color: #0a4459;
+`;
+const AddressText = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #8aa1aa;
+`;
+const AddressPhoneNumber = styled.div`
+  margin-top: 12px;
+  margin-bottom: 18px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #8aa1aa;
+`;
+const AddressButton = styled.button`
+  margin-bottom: 8px;
+  width: 100%;
+  height: 40px;
+  border: 1px solid #dee8ec;
+  border-radius: 2px;
+  background-color: #ffffff;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
+  color: #0a4459;
+  cursor: pointer;
+  &:nth-of-type(3) {
+    margin-bottom: 0px;
+  }
+`;
+const RadioButton = styled.input`
+  display: none;
+`;
+
+const RadioLabel = styled.label<{ isChecked: boolean }>`
+  display: flex;
+  align-items: center;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
+  color: #0a4459;
+`;
+const DefaultCircle = styled.label<{ isChecked: boolean }>`
+  display: ${(props) => {
+    return props.isChecked == true ? "none" : "block";
+  }};
+  margin-right: 8px;
+  width: 16px;
+  height: 16.65px;
+  border: 1px solid #dee8ec;
+  border-radius: 42.6667px;
+  box-sizing: border-box;
+`;
+const CheckedCircle = styled.label<{ isChecked: boolean }>`
+  display: ${(props) => {
+    return props.isChecked == true ? "flex" : "none";
+  }};
+  align-items: center;
+  justify-content: center;
+  margin-right: 8px;
+  width: 16px;
+  height: 16.65px;
+  background-color: #ff5c01;
+  border-radius: 8px;
+`;
+const SmallCircle = styled.div`
+  height: 6px;
+  width: 6px;
+  background-color: #ffffff;
+  border-radius: 100%;
+`;
+const DashLine = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  border-bottom: 1px dashed #dee8ec;
+`;
+const CheckBox = styled.input`
+  display: none;
+`;
+const CheckBoxLabel = styled.label<{ isChecked: boolean }>`
+  display: flex;
+  margin-bottom: ${(props) => {
+    return props.isChecked == true ? "0" : "15px";
+  }};
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #0a4459;
+`;
+const Box = styled.div<{ isChecked: boolean; img: string }>`
+  margin-right: 8px;
+  width: 16px;
+  height: 16px;
+  box-sizing: border-box;
+
+  border: 1px solid #dee8ec;
+  border-radius: 2.66667px;
+
+  background-color: ${(props) => {
+    return props.isChecked == true ? "#FF5C01" : "#FFFFFF";
+  }};
+
+  background-image: url(${(props) => {
+    return props.isChecked == true ? props.img : "";
+  }});
+  background-size: 9.5px 7.4px;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const PriceWrapper = styled.div`
+  margin-bottom: 24px;
+  padding-top: 15px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-bottom: 14px;
+  background: #f2f6f8;
+  border: 0.79402px solid #dee8ec;
+  border-radius: 2px;
+`;
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-itmes: center;
+  margin-bottom: 2px;
+`;
+const PriceTitle = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #8aa1aa;
+`;
+const QuestionMark = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 4px;
+  background-color: #8aa1aa;
+  width: 12px;
+  height: 12px;
+  border-radius: 100%;
+  font-weight: 700;
+  font-size: 9px;
+  line-height: 9px;
+  letter-spacing: -0.011em;
+  color: #ffffff;
+`;
+const Price = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #8aa1aa;
+`;
+const Line = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  border-bottom: 1px solid #dee8ec;
+`;
+const TotalTitle = styled.div`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: -0.011em;
+  color: #0a4459;
+`;
+const TotalPrice = styled.div`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  color: #ff5c01;
+`;
+const ButtonWrapper = styled.div`
+  margin: 0 auto;
+  margin-bottom: 16px;
+  max-width: 427px;
+  display: flex;
+  gap: 10.5px;
+  @media screen and (max-width: 767px) {
+    padding-left: 20px;
+    padding-right: 20px;
+    max-width: 100%;
+  }
+`;
+const CancelButton = styled.button`
+  width: 100%;
+  height: 48px;
+  background-color: #ffffff;
+  border: 1px solid #dee8ec;
+  border-radius: 2px;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  color: #0a4459;
+  cursor: pointer;
+`;
+const CheckoutButton = styled.button`
+  width: 100%;
+  height: 48px;
+  background-color: #ff5c01;
+  border: none;
+  border-radius: 2px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+export default useCheck_out;
