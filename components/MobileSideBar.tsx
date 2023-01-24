9c8ffdaf4b1100280ae4e831d77cc75e4e767d1b
joseph-wee <starlightkor@gmail.com>
2023-01-24 13:24:25 +0900

    design: 모바일전용 사이드바 마크업

---
+++ components/MobileSideBar.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import styled from "styled-components";
+
+const MobileSideBar = () => {
+  const router = useRouter();
+  return (
+    <Container>
+      <Line />
+      <Link href="/favorits" style={{ textDecoration: "none" }}>
+        <MenuTitle>Favorits</MenuTitle>
+      </Link>
+      <Line />
+      <Link href="/cart" style={{ textDecoration: "none" }}>
+        <MenuTitle>Cart</MenuTitle>
+      </Link>
+      <Line />
+      <Link href="/order" style={{ textDecoration: "none" }}>
+        <MenuTitle>Order</MenuTitle>
+      </Link>
+      <Line />
+      <Link href="/order_history" style={{ textDecoration: "none" }}>
+        <MenuTitle>Order History</MenuTitle>
+      </Link>
+      <Line />
+      <Link href="/address" style={{ textDecoration: "none" }}>
+        <MenuTitle>Address</MenuTitle>
+      </Link>
+      <Line />
+      <Link href="/account_detail" style={{ textDecoration: "none" }}>
+        <MenuTitle>Account Detail</MenuTitle>
+      </Link>
+      <Line />
+      <MenuTitle onClick={() => router.push("/home")}>Logout</MenuTitle>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: none;
+  @media screen and (max-width: 767px) {
+    display: block;
+  }
+`;
+const Line = styled.div`
+  border-bottom: 1px solid #dee8ec;
+`;
+const MenuTitle = styled.div`
+  display: inline-block;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 14px;
+  letter-spacing: -0.011em;
+  color: #0a4459;
+  cursor: pointer;
+`;
+
+export default MobileSideBar;
