c217b5be115d391f44431b85b9314a8a96993ea2
joseph-wee <starlightkor@gmail.com>
2023-01-24 13:20:36 +0900

    design: recent orders 바 마크업

---
+++ components/RecentOrders.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const RecentOrders = () => {
+  return <Container>Recent orders 1</Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  padding-left: 16px;
+  height: 30px;
+  background: #f2f6f8;
+  border: 0.79402px solid #dee8ec;
+  border-radius: 2px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
+  color: #0a4459;
+`;
+
+export default RecentOrders;
