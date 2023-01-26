e6408a954dc4086b0003e27d705a7425147e603b
joseph-wee <starlightkor@gmail.com>
2023-01-26 19:02:54 +0900

    design: 푸터위치 변경을 위해 글로벌 스타일 변경

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -4,8 +4,12 @@ import { reset } from "styled-reset";
 export const GlobalStyle = createGlobalStyle`
     ${reset}
     html {
+        position: relative;
         font-family: 'Roboto';
         padding-top: 64px;
+        padding-bottom: 114.35px;
+        min-height: 100vh;
+        box-sizing: border-box;
     }
     * {
         font-family: 'Roboto';
