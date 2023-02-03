5652949c884f7d84537129f97ac58f5d36533f4b
joseph-wee <starlightkor@gmail.com>
2023-02-03 11:52:30 +0900

    feat: 로그인 api call 이후 응답에 따라 액션 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,57 +6,78 @@ import axios from "axios";
 import { ic_check_wht } from "../assets";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { loginRequest } from "../utils/api";
+import { PopUp } from "../components";
+import { useAppDispatch, useAppSelector } from "../pages/redux/hooks";
+import { login } from "../features/login/loginSlice";
 
 const useLogin = () => {
   const [userId, setUserId] = useState<string>("");
   const [password, setPaswword] = useState<string>("");
   const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [authPageIsActive, setAuthPageIsActive] = useState<boolean>(false);
+  const [popUpIsActive, setPopUpIsActive] = useState<boolean>(false);
+  const { value: isLogin } = useAppSelector((state) => state.isLogin);
 
+  const dispatch = useAppDispatch();
   const router = useRouter();
 
-  /** 로그인 요청 api */
-  const loginApiRequest = () => {
-    axios({
-      method: "POST",
-      url:
-        process.env.NEXT_PUBLIC_API_KEY +
-        `login?userId=${userId}&password=${password}&role=USER`,
-    })
-      .then(function (response) {
-        if (response.data.status == 200) {
+  // /**값 비어있는지 검사 후에 로그인 요청 api 호출 */
+  // const loginApiRequestHandler = () => {
+  //   if (userId.length == 0) {
+  //     alert("값을 모두 채워주세요");
+  //   } else if (password.length == 0) {
+  //     alert("값을 모두 채워주세요");
+  //   } else {
+  //     // loginApiRequest();
+  //     router.push("/authentication");
+  //   }
+  // };
+
+  /** 로그인 api 요청후 결과에 따라 액션 */
+  const loginRequestHandler = (userId: string, password: string) => {
+    loginRequest(userId, password).then((res) => {
+      if (Boolean(res?.data)) {
+        if (res?.data.status == 200) {
           alert("로그인 성공하였습니다.(임시 메세지)");
+          sessionStorage.setItem("at", res.data.result.access_token);
+          sessionStorage.setItem("rt", res.data.result.refresh_token);
           router.push("/");
-        } else if (response.data.status == 401) {
+        } else if (res?.data.status == 401) {
           alert("로그인에 실패하였습니다.(임시 메세지)");
         }
-      })
-      .catch(function (error) {
-        alert("통신 실패(임시 메세지)");
-
-        console.log(error);
-      });
+      } else if (res?.response.data.status == 403) {
+        setAuthPageIsActive(true);
+      }
+    });
   };
 
-  /**값 비어있는지 검사 후에 로그인 요청 api 호출 */
-  const loginApiRequestHandler = () => {
-    if (userId.length == 0) {
-      alert("값을 모두 채워주세요");
-    } else if (password.length == 0) {
-      alert("값을 모두 채워주세요");
-    } else {
-      // loginApiRequest();
-      router.push("/authentication");
-    }
+  /** 인증 확인버튼 클릭 시 확인 유무에따라 팝업 혹은 페이지 이동 */
+  const authConfirmHandler = (userId: string, password: string) => {
+    loginRequest(userId, password).then((res) => {
+      if (Boolean(res?.data)) {
+        if (res?.data.status == 200) {
+          sessionStorage.setItem("at", res.data.result.access_token);
+          sessionStorage.setItem("rt", res.data.result.refresh_token);
+          dispatch(login());
+          router.push("/");
+        } else if (res?.data.status == 401) {
+          // 무슨 에러 처리를 해야할까?
+        }
+      } else if (res?.response.data.status == 403) {
+        setPopUpIsActive(true);
+      }
+    });
   };
 
-  useEffect(() => {
-    console.log(isChecked);
-    console.log(ic_check_wht.src);
-  }, [isChecked]);
+  // useEffect(() => {
+  //   console.log(isChecked);
+  //   console.log(ic_check_wht.src);
+  // }, [isChecked]);
 
   return (
     <>
-      <Container>
+      <Container isActive={authPageIsActive}>
         <Title>Login</Title>
         <WelcomeText>Welcome to Repunch</WelcomeText>
 
@@ -86,7 +107,9 @@ const useLogin = () => {
             <LinkStyling2>Lost Password? </LinkStyling2>
           </Link>
         </Wrapper>
-        <Button onClick={() => loginApiRequestHandler()}>Confirm</Button>
+        <Button onClick={() => loginRequestHandler(userId, password)}>
+          Confirm
+        </Button>
         <RegisterContainer>
           <RegisterTitle>Register</RegisterTitle>
           <Text>
@@ -101,11 +124,48 @@ const useLogin = () => {
           </ButtonRegister>
         </RegisterContainer>
       </Container>
+      <AuthContainer isActive={authPageIsActive}>
+        <AuthWrapper>
+          <AuthTitle>Repunch</AuthTitle>
+          <Name>J Kim</Name>
+          <ConfirmWrapper>
+            <Id>ID (E-mail)</Id>
+            <Email>{userId}</Email>
+            <AuthText>
+              An authentication email has been sent to
+              <br />
+              your email address.
+              <br />
+              You can use all services freely after the
+              <br />
+              authentication process.
+            </AuthText>
+            <AuthButton onClick={() => authConfirmHandler(userId, password)}>
+              Confirm
+            </AuthButton>
+          </ConfirmWrapper>
+        </AuthWrapper>
+        <TextInform>
+          If you entered the wrong email address, please
+          <br />
+          contact the email below
+        </TextInform>
+        <EmailRepunch>support@repunch.co.kr</EmailRepunch>
+      </AuthContainer>
+      <PopUp
+        title={"Not verified yet"}
+        text={"Wait a little longer or resend"}
+        isActive={popUpIsActive}
+        setIsActive={setPopUpIsActive}
+      />
     </>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ isActive: boolean }>`
+  display: ${(props) => {
+    return props.isActive == true ? "none" : "block";
+  }};
   position: relative;
   margin: 0 auto;
   padding-top: 20px;
@@ -315,6 +375,144 @@ const LinkStyling = styled.div`
   font-weight: 400;
   color: #ffffff;
   line-height: 130%;
+`;
+
+const AuthContainer = styled.div<{ isActive: boolean }>`
+  display: ${(props) => {
+    return props.isActive == true ? "block" : "none";
+  }};
+  padding-top: 40px;
+  padding-bottom: 40px;
+  padding-right: 20px;
+  padding-left: 20px;
+  @media screen and (max-width: 767px) {
+    padding-top: 20px;
+  }
+`;
+const AuthWrapper = styled.div`
+  margin: 0 auto;
+  margin-bottom: 29px;
+  padding-top: 15px;
+  padding-bottom: 20px;
+  padding-right: 20px;
+  padding-left: 20px;
+  background-color: #f2f6f8;
+  max-width: 427px;
+  border: 0.79402px solid #dee8ec;
+  border-radius: 2px;
+  box-sizing: border-box;
+  @media screen and (max-width: 767px) {
+    padding-top: 11px;
+  }
+`;
+const AuthTitle = styled.div`
+  display: flex;
+  margin-bottom: 4px;
+  align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 18px;
+
+  color: #0a4459;
+`;
+const Name = styled.div`
+  display: flex;
+  margin-bottom: 16px;
+  align-items: center;
+  justify-content: center;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+
+  color: #1eab92;
+`;
+const ConfirmWrapper = styled.div`
+  padding-top: 16px;
+  padding-bottom: 20px;
+  background-color: #ffffff;
+
+  border: 1px solid #dee8ec;
+  border-radius: 2px;
+  box-sizing: border-box;
+`;
+const Id = styled.div`
+  display: flex;
+  align-itmes: center;
+  justify-content: center;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+
+  color: #0a4459;
+`;
+const Email = styled.div`
+  display: flex;
+  margin-bottom: 12px;
+  align-itmes: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+
+  color: #ff5c01;
+`;
+const AuthText = styled.div`
+  margin-bottom: 12px;
+  text-align: center;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
+
+  color: #0a4459;
+`;
+
+const AuthButton = styled.button`
+  display: flex;
+  margin: 0 auto;
+  width: 180px;
+  height: 40px;
+
+  box-sizing: border-box;
+
+  align-items: center;
+  justify-content: center;
+
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 18px;
+
+  color: #ffffff;
+
+  overflow: hidden;
+
+  background-color: #1eab92;
+  border: none;
+  border-radius: 2px;
+
+  cursor: pointer;
+`;
+
+const TextInform = styled.div`
+  margin-bottom: 6px;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
+
+  text-align: center;
+
+  color: #8aa1aa;
+`;
+const EmailRepunch = styled.div`
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px%;
+
+  text-align: center;
+  text-decoration-line: underline;
+
+  color: #0a4459;
 `;
 
 export default useLogin;
