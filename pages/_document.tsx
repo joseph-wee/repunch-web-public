b74d81574e4b9427c638acc86a4bfeb23f232bbd
joseph-wee <starlightkor@gmail.com>
2023-06-16 18:36:51 +0900

    del: 페이팔 script코드 삭제

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,7 +46,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
         <body>
           <Main />
           <NextScript />
