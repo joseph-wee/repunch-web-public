7fb092204ff57c0617dc8e21692fc49826b48734
joseph-wee <starlightkor@gmail.com>
2023-07-21 16:59:48 +0900

    set: next-image를 이용한 외부 이미지 소스 호출 config세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'punch-image.s3.us-west-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd1hatg1wharzcs.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   reactStrictMode: true,
+
 }
 
 module.exports = nextConfig
