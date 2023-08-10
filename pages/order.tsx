a445e3219b7d3c42cf47b10323088a39f0546140
joseph-wee <starlightkor@gmail.com>
2023-08-10 14:35:40 +0900

    feat: 주문 페이지 버튼 클릭시 해당 주문만 보이게 수정

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {
-  MobileSideBar,
-  OrderInfoBox,
-  RecentOrders,
-  SideBar,
-} from "../components";
+import { MobileSideBar, OrderInfoBox, SideBar } from "../components";
 import Link from "next/link";
 import Image from "next/image";
 import { btn_web_back } from "../assets";
 import { goBack } from "../utils/functions";
 
 const order = () => {
+  const [clicked, setClicked] = useState(1);
+
   return (
     <Container>
       <SideBar />
@@ -23,17 +20,37 @@ const order = () => {
           <Title>Order</Title>
         </TitleWrapper>
         <ButtonWrapper>
-          <AllButton>ALL</AllButton>
-          <ReviewButton>In Review (0)</ReviewButton>
-          <ConfirmButton>Order confirmed (0)</ConfirmButton>
-          <ShipButton>Shipped (1)</ShipButton>
-          <DeliveredButton>Delivered (0)</DeliveredButton>
-          <PickupButton>Pick up (0)</PickupButton>
-          <CanceledButton>Canceled (0)</CanceledButton>
-          <ReturnButton>Return (0)</ReturnButton>
+          <AllButton onClick={() => setClicked(1)} clicked={clicked}>
+            ALL
+          </AllButton>
+          <ReviewButton onClick={() => setClicked(2)} clicked={clicked}>
+            In Review (0)
+          </ReviewButton>
+          <ConfirmButton onClick={() => setClicked(3)} clicked={clicked}>
+            Order confirmed (0)
+          </ConfirmButton>
+          <ShipButton onClick={() => setClicked(4)} clicked={clicked}>
+            In production (0)
+          </ShipButton>
+          <DeliveredButton onClick={() => setClicked(5)} clicked={clicked}>
+            Shipped (1)
+          </DeliveredButton>
+          <PickupButton onClick={() => setClicked(6)} clicked={clicked}>
+            Delivered (0)
+          </PickupButton>
+          <CanceledButton onClick={() => setClicked(7)} clicked={clicked}>
+            Pick up (0)
+          </CanceledButton>
         </ButtonWrapper>
-        <RecentOrders />
-        <OrderInfoBox accomplish={false} myAccount={false} />
+
+        {clicked == 1 || clicked == 5 ? (
+          <>
+            <RecentOrders>Recent orders 1</RecentOrders>
+            <OrderInfoBox accomplish={false} myAccount={false} />
+          </>
+        ) : (
+          <RecentOrders>Recent orders 0</RecentOrders>
+        )}
       </Main>
       <MobileSideBar />
     </Container>
@@ -99,89 +116,180 @@ const ButtonWrapper = styled.div`
   flex-wrap: wrap;
   margin-bottom: 20px;
 `;
-const AllButton = styled.button`
+const AllButton = styled.button<{ clicked: number }>`
   width: 47px;
   height: 36px;
   background-color: #ffffff;
-  border: 1px solid #121822;
+  ${(props) => {
+    switch (props.clicked) {
+      case 1:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 700;
   font-size: 12px;
   line-height: 14px;
   color: #121822;
+  cursor: pointer;
 `;
-const ReviewButton = styled.button`
+const ReviewButton = styled.button<{ clicked: number }>`
   width: 87px;
   height: 36px;
   background-color: #ffffff;
-  border: 1px solid #dee8ec;
+  ${(props) => {
+    switch (props.clicked) {
+      case 2:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 400;
   font-size: 12px;
   line-height: 14px;
   color: #121822;
+  cursor: pointer;
 `;
-const ConfirmButton = styled.button`
-  width: 120px;
+const ConfirmButton = styled.button<{ clicked: number }>`
+  width: 122px;
   height: 36px;
   background-color: #ffffff;
-  border: 1px solid #dee8ec;
+  ${(props) => {
+    switch (props.clicked) {
+      case 3:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 400;
   font-size: 12px;
   line-height: 14px;
   color: #121822;
+  cursor: pointer;
 `;
-const ShipButton = styled.button`
+const ShipButton = styled.button<{ clicked: number }>`
+  width: 104px;
+  height: 36px;
+  background-color: #ffffff;
+  ${(props) => {
+    switch (props.clicked) {
+      case 4:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
+  border-radius: 2px;
+  box-sizing: border-box;
+  font-size: 12px;
+  line-height: 14px;
+  color: #121822;
+  cursor: pointer;
+`;
+const DeliveredButton = styled.button<{ clicked: number }>`
   width: 84px;
   height: 36px;
   background-color: #ffffff;
-  border: 1px solid #dee8ec;
+  ${(props) => {
+    switch (props.clicked) {
+      case 5:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 400;
   font-size: 12px;
   line-height: 14px;
   color: #121822;
+  cursor: pointer;
 `;
-const DeliveredButton = styled.button`
+const PickupButton = styled.button<{ clicked: number }>`
   width: 93px;
   height: 36px;
   background-color: #ffffff;
-  border: 1px solid #dee8ec;
+  ${(props) => {
+    switch (props.clicked) {
+      case 6:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 400;
   font-size: 12px;
   line-height: 14px;
   color: #121822;
+  cursor: pointer;
 `;
-const PickupButton = styled.button`
+const CanceledButton = styled.button<{ clicked: number }>`
   width: 79px;
   height: 36px;
   background-color: #ffffff;
-  border: 1px solid #dee8ec;
+  ${(props) => {
+    switch (props.clicked) {
+      case 7:
+        return `
+        border: 1px solid #121822;
+        font-weight: 700;
+        `;
+      default:
+        return `
+          border: 1px solid #DEE8EC;
+          font-weight: 400;
+          `;
+    }
+  }};
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 400;
   font-size: 12px;
   line-height: 14px;
   color: #121822;
-`;
-const CanceledButton = styled.button`
-  width: 93px;
-  height: 36px;
-  background-color: #ffffff;
-  border: 1px solid #dee8ec;
-  border-radius: 2px;
-  box-sizing: border-box;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  color: #121822;
+  cursor: pointer;
 `;
 const ReturnButton = styled.button`
   width: 93px;
@@ -190,9 +298,23 @@ const ReturnButton = styled.button`
   border: 1px solid #dee8ec;
   border-radius: 2px;
   box-sizing: border-box;
-  font-weight: 400;
   font-size: 12px;
   line-height: 14px;
+  color: #121822;
+`;
+
+const RecentOrders = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  padding-left: 16px;
+  height: 30px;
+  background: #f2f6f8;
+  border: 0.79402px solid #dee8ec;
+  border-radius: 2px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
   color: #121822;
 `;
 
