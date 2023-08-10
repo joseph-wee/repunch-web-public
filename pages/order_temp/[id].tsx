4c95652c593e46d510733bf35820f60b563de810
joseph-wee <starlightkor@gmail.com>
2023-08-10 14:32:51 +0900

    design: 필요없는 add new address 버튼 제거

--- pages/order_temp/[id].tsx
+++ pages/order_temp/[id].tsx
@@ -76,10 +76,6 @@ const useOrder_temp = () => {
           <AddressText>korea</AddressText>
           <AddressText>06285</AddressText>
           <AddressPhoneNumber>+82 1086281024</AddressPhoneNumber>
-          <AddressButton>+ Add a new address</AddressButton>
-          <AddressButton onClick={() => setPopUpIsActive(1)}>
-            Select other address
-          </AddressButton>
         </ContentWrapper>
         <ContentTitle>Delivery</ContentTitle>
         <ContentWrapper>
@@ -330,7 +326,6 @@ const AddressText = styled.div`
 `;
 const AddressPhoneNumber = styled.div`
   margin-top: 12px;
-  margin-bottom: 18px;
   font-weight: 400;
   font-size: 12px;
   line-height: 16px;
