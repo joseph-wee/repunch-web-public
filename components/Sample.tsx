d662d2eecaeee6dffb302df7aefa97473200611e
joseph <joseph@josephui-MacBookPro.local>
2023-10-05 18:02:10 +0900

    feat: 샘플 주문 컴포넌트 썸네일 데이터 연결

--- components/Sample.tsx
+++ components/Sample.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
-import { test_thumbnail } from "../assets";
 
-const Sample = () => {
+const Sample = ({ data }: any) => {
   return (
     <ImageWrapper>
-      <Image src={test_thumbnail} alt={"test"} width={48} height={48} />
+      <Image src={data.thumbnail} alt={"test"} width={48} height={48} />
     </ImageWrapper>
   );
 };
