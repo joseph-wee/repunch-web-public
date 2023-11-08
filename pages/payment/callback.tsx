81a96b47d8c70cbe870c1f7f183128ace5afc9b2
joseph <joseph@josephui-MacBookPro.local>
2023-11-08 17:15:25 +0900

    feat: 임시 주문결제 2단계 연동

--- pages/payment/callback.tsx
+++ pages/payment/callback.tsx
@@ -1,12 +1,21 @@
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import { paymentRequest2 } from "../../utils/api";
 
 const useCallback = () => {
   const router = useRouter();
 
+  const paymentHandler = () => {
+    paymentRequest2(localStorage.getItem("at")).then((res) => {
+      console.log(res);
+    });
+  };
+
   useEffect(() => {
     const token = router.asPath.split("&")[0].split("token=")[1];
     const PayerID = router.asPath.split("&")[1].split("PayerID=")[1];
+
+    paymentHandler();
   }, []);
   return <div>callback</div>;
 };
