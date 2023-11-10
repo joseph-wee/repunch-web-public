1b1e9ef6be59e43a44f67d2bed95adb6f5d38863
joseph <joseph@josephui-MacBookPro.local>
2023-11-10 18:55:31 +0900

    add: mp4 재생을 위한 라이브러리 설치 및 config 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withVideos = require("next-videos");
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -32,4 +34,4 @@ const nextConfig = {
 
 }
 
-module.exports = nextConfig
+module.exports = withVideos(nextConfig)
