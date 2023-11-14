df618f3790d0f98e55b2a861948cfd82dffcb9c0
joseph <joseph@josephui-MacBookPro.local>
2023-11-14 16:15:11 +0900

    add: about us 페이지 전용 헤더바 추가

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 export { default as FooterBar } from "./FooterBar";
 export { default as HeaderBar } from "./HeaderBar";
+export { default as HeaderBarAboutUs } from "./HeaderBarAboutUs";
 export { default as SelectBox } from "./SelectBox";
 export { default as SelectBoxCountryCodeNum } from "./SelectBoxCountryCodeNum";
 export { default as NavTopBar } from "./NavTopBar";
