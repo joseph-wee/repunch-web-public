5cdf6e899e5a6547069726e805d76ec142a4af9d
joseph <joseph@josephui-MacBookPro.local>
2023-11-24 10:24:39 +0900

    feat: 상품 목록 8개씩 호출 및 무한 스크롤 구현

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,40 +1,65 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import {
-  btn_favorite_inact,
-  btn_review,
-  ic_favorite_wht,
-  test_thumbnail,
-  test_thumbnail_green,
-  test_thumbnail_red,
-} from "../assets";
-import Image from "next/legacy/image";
-import Link from "next/link";
-import { productsRequestNext, productsRequestFirst } from "../utils/api";
+import { is } from "immer/dist/internal";
+import React, { useEffect, useRef, useState } from "react";
+import { productsRequest } from "../utils/api";
 import Product from "./Product";
 
-const ProductList = () => {
-  const [productList, setProductList] = useState([]);
+const ProductList = ({ sortType }: any) => {
+  const [productList, setProductList] = useState<any>([]);
 
-  /** 상품 리스트 호출 함수 */
-  const productListRequestFirst = (searchAfter: number) => {
-    productsRequestFirst(50).then((res) => {
-      let tempArr = productList;
-      tempArr = res.data.result.data;
-      setProductList([...tempArr]);
-      console.log(tempArr);
+  const [searchAfter, setSearchAfter] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const ref = useRef<any>();
+
+  /** 상품 리스트 초기화 후 호출 함수 */
+  const productListRequestInitHandler = () => {
+    let at = localStorage.getItem("at");
+    let rt = localStorage.getItem("rt");
+
+    productsRequest(at, sortType, 8, null).then((res) => {
+      let x = res?.data.result.data;
+      setProductList([...x]);
+      setSearchAfter(x[x.length - 1].productNo);
+      setLoading(false);
     });
   };
 
+  /** 상품 리스트 호출 함수 */
+  const productListRequestAdditionalHandler = () => {
+    productsRequest(null, sortType, 8, searchAfter).then((res) => {
+      if (res?.data.result.data) {
+        let x = res?.data.result.data;
+        setProductList([...productList, ...x]);
+        setSearchAfter(x[x.length - 1].productNo);
+        console.log(x[x.length - 1].productNo);
+        setLoading(false);
+        return;
+      }
+    });
+  };
+
+  /** sortType 바뀌면 productList초기화 후 다시 상품 리스트 호출 */
   useEffect(() => {
-    productListRequestFirst(50);
-  }, []);
+    productListRequestInitHandler();
+  }, [sortType]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+      isIntersecting && !loading && productListRequestAdditionalHandler();
+    });
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [loading, searchAfter]);
 
   return (
     <>
-      {productList.map((i: any, j: number) => {
-        return <Product product={i} key={`product${j}`} />;
-      })}
+      {productList &&
+        productList.map((i: any, j: number) => {
+          return <Product product={i} key={`product${j}`} />;
+        })}
+      {productList && <div ref={ref}></div>}
     </>
   );
 };
