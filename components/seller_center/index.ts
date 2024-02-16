93343c38072e77c4a50a7abc192b7fd2a9c2302a
joseph-wee <starlightkor@gmail.com>
2024-02-16 16:17:08 +0900

    add: 셀러센터 컴포넌트 인덱싱

---
+++ components/seller_center/index.ts
@@ -0,0 +1,6 @@
+export { default as PopUpSelectColor } from "./PopUpSelectColor";
+export { default as PopUpSelectComposition } from "./PopUpSelectComposition";
+export { default as PopUpSelectCountry } from "./PopUpSelectCountry";
+export { default as PopUpSelectDesign } from "./PopUpSelectDesign";
+export { default as PopUpSelectProject } from "./PopUpSelectProject";
+export { default as PopUpSelectWidth } from "./PopUpSelectWidth";
