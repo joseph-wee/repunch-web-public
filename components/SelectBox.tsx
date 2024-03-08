fbb0da4fdcf4a7a6b2ffcad3678de047ced5ca27
joseph <joseph@josephui-MacBookPro.local>
2024-03-08 17:56:02 +0900

    feat: 국가 셀렉트 박스 방향키 기능 구현

--- components/SelectBox.tsx
+++ components/SelectBox.tsx
@@ -1,6 +1,6 @@
 /* ------------- 셀렉트박스 컴포넌트 ------------- */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import { arrow_down, arrow_up } from "../assets";
@@ -24,6 +24,8 @@ const SelectBox = ({
 }) => {
   const [isActive, setIsActive] = useState<boolean>(false); // 셀렉트박스 활성 유무
   const [text, setText] = useState<any>(""); // 선택된 값이 보여지는 텍스트
+  const [focusIndex, setFocusIndex] = useState(-1);
+  const ref = useRef<HTMLDivElement>(null);
 
   /** 옵션 선택하면 해당 값이 세팅 */
   const optionHandler = (i: any) => {
@@ -70,15 +72,77 @@ const SelectBox = ({
     setResult([...tempResult]);
   }, [text]);
 
-  useEffect(() => {
-    console.log(value);
-  }, [value]);
-
   /** 영어만 허용 */
   const charBlocker = (e: React.ChangeEvent<HTMLInputElement>) => {
     const reg = /[^A-Za-z\s]/gi;
     e.target.value = e.target.value.replace(reg, "");
   };
+
+  /** 위, 아래 방향키로 나라 포커싱 */
+  const arrowHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    //ArrowDown
+    //ArrowUp
+
+    const scrollTop = Number(ref.current?.scrollTop);
+
+    // ref.current?.scrollTo({ top: ref.current?.scrollTop + 40 });
+    // enter 누르면 해당 나라 선택
+    if (e.key === "Enter") {
+      optionHandler(result[focusIndex]);
+    }
+
+    // 7개 이하면
+    if (result.length <= 7) {
+      // 윗 방향키인데 -1이면 return
+      if (e.key === "ArrowUp" && focusIndex === -1) {
+        return;
+      }
+      // 아래 방향키인데 result길이 끝이면 return
+      if (e.key === "ArrowDown" && focusIndex === result.length) {
+        return;
+      }
+      // 위 방향키이면 -1
+      if (e.key === "ArrowUp") {
+        setFocusIndex((prev) => prev - 1);
+        return;
+      }
+      // 아래 방향키이면 +1
+      if (e.key === "ArrowDown") {
+        setFocusIndex((prev) => prev + 1);
+        return;
+      }
+    }
+
+    // 윗 방향키인데 -1이면 return
+    if (e.key === "ArrowUp" && focusIndex === -1) {
+      return;
+    }
+    // 아래 방향키인데 result길이 끝이면 return
+    if (e.key === "ArrowDown" && focusIndex === result.length) {
+      return;
+    }
+    // 위 방향키이면 -1
+    if (e.key === "ArrowUp") {
+      scrollTop > (focusIndex - 1) * 40 &&
+        ref.current?.scrollTo({ top: scrollTop - 40 });
+      setFocusIndex((prev) => prev - 1);
+      // scrolltop
+
+      return;
+    }
+    // 아래 방향키이면 +1
+    if (e.key === "ArrowDown") {
+      // scrollTop + 240 < focusIndex * 40
+      scrollTop + 240 < (focusIndex + 1) * 40 &&
+        ref.current?.scrollTo({ top: scrollTop + 40 });
+      setFocusIndex((prev) => prev + 1);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    console.log(ref.current?.scrollTop);
+  }, [ref]);
 
   return (
     <>
@@ -89,19 +153,28 @@ const SelectBox = ({
             onChange={(e) => {
               charBlocker(e);
               setText(e.target.value);
+              setFocusIndex(-1);
             }}
             value={text}
             onFocus={() => setIsActive(true)}
-            onBlur={() => setIsActive(false)}
+            onBlur={() => {
+              setIsActive(false);
+              setFocusIndex(-1);
+            }}
             placeholder="Input your country"
             maxLength={50}
+            onKeyDown={(e) => arrowHandler(e)}
           />
 
           <ImageWrapper>
             <Image src={isActive ? arrow_up : arrow_down} alt="arrow" />
           </ImageWrapper>
         </Select>
-        <OptionWrapper isActive={isActive} resultLength={result.length}>
+        <OptionWrapper
+          isActive={isActive}
+          resultLength={result.length}
+          ref={ref}
+        >
           {result &&
             result.map((i: any, j: number) => {
               let startIndex = 0;
@@ -113,6 +186,8 @@ const SelectBox = ({
                 <Option
                   key={`${j}aasccpas-dc`}
                   onMouseDown={() => optionHandler(i)}
+                  index={j}
+                  focusIndex={focusIndex}
                 >
                   {i.name.split("").map((el: string, index: number) => {
                     return index >= startIndex &&
@@ -200,7 +275,7 @@ const OptionWrapper = styled.div<{ isActive: boolean; resultLength: number }>`
   cursor: default;
 `;
 
-const Option = styled.div`
+const Option = styled.div<{ index: number; focusIndex: number }>`
   display: flex;
   padding-left: 16px;
   width: 100%;
@@ -212,10 +287,12 @@ const Option = styled.div`
   font-weight: 400;
   line-height: 14px;
 
-  background-color: #ffffff;
   &:last-of-type {
     border: none;
   }
+  background-color: ${(props) => {
+    return props.focusIndex === props.index ? "#F5F8F9" : "#ffffff";
+  }};
 `;
 
 const Bold = styled.span`
