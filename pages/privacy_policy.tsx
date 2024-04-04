cd55b28454125843decb72aa3f357c73100b92cc
joseph-wee <starlightkor@gmail.com>
2024-04-05 01:13:35 +0900

    design: privacy policy 페이지 작업

--- pages/privacy_policy.tsx
+++ pages/privacy_policy.tsx
@@ -5,87 +5,216 @@ const privacy_policy = () => {
   return (
     <Container>
       <Title>Privacy policy</Title>
-      <Line />
-      <Text>
-        Tujuan Ketentuan Layanan ini adalah untuk mengatur hak, kewajiban,
-        tanggung jawab dan hal-hal lain yang diperlukan antara PT. JOOB GLOBAL
-        INDONESIA (&quot;Perseroan&quot;) dan &quot;Pengguna&quot; (sebagaimana
-        didefinisikan di bawah) sehubungan dengan penggunaan JOOB, layanan
-        aplikasi seluler yang disediakan oleh Perseroan.
-        <br />1 – Definisi
-      </Text>
+      <Text>1. Introduction</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          &quot;Bisnis&quot; mengacu pada entitas tempat lowongan pekerjaan
-          didaftarkan dengan tujuan mencari kandidat untuk pekerjaan dan
-          menghubungkan Pemilik Bisnis, Manajer Bisnis, dan dengan calon
-          Karyawan.
+          Welcome to Repunch, an online fabric-selling website located in South
+          Korea. In this Privacy Policy, we, at Repunch, are committed to
+          safeguarding your privacy and protecting your personal data. This
+          Privacy Policy explains how we collect, use, and protect your personal
+          information when you use our website.
         </Text>
       </Wrapper>
+
+      <Text>2. What Is Personal Data?</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          &quot;Pemilik Bisnis&quot; mengacu pada otoritas yang dapat
-          mendaftarkan Bisnis, menambah, mengedit, dan menghapus posting
-          pekerjaan, serta mengelola daftar Karyawan dan Manajer Bisnis.
+          Personal Data refers to any information that can identify you directly
+          or indirectly. This includes but is not limited to your name, email
+          address, phone number, and IP address. It may also encompass
+          information related to your professional function or your interactions
+          with our website.
         </Text>
       </Wrapper>
+
+      <Text>3. Data Controller</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          &quot;Manajer Bisnis&quot; mengacu pada otoritas yang dapat menambah,
-          mengedit, dan menghapus posting pekerjaan dan mengelola daftar
-          Karyawan.
+          Repunch is the data controller responsible for managing and protecting
+          your data. If you have any questions, concerns, or requests related to
+          your personal data, please contact us at:
+          <br />
+          Email: [Your Email Address]
+          <br />
+          Postal Address: [Your Company&apos;s Address in South Korea]
         </Text>
       </Wrapper>
+
+      <Text>4. Why and How Do We Use Your Data?</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          &quot;Pencari Kerja&quot; mengacu pada orang yang mengakses layanan
-          JOOB untuk mencari pekerjaan.
+          Collection of Data
+          <br />
+          We collect your personal data in the following ways: When you provide
+          information directly to us through forms on our website, including
+          order processing, customer inquiries, and after-sale services. Through
+          automated means when you access our website, including the use of
+          cookies, which may collect technical information about your device and
+          browsing activity. From third parties, such as social media platforms,
+          when you interact with us through these platforms.
+          <br />
+          Types of Data We Collect
+          <br />
+          Repunch may collect the following types of personal data: Information
+          required to process your orders, including selected items, delivery
+          and billing addresses, and contact details. Data related to after-sale
+          services, such as returns, exchanges, and repair requests. Information
+          about your preferences, such as liked products and services. Records
+          of your interactions with our customer service, including recorded
+          calls for quality assurance. Please note that fields marked as
+          mandatory in data collection forms must be completed to fulfill your
+          requests or orders.
+          <br />
+          Legal Basis for Data Collection
+          <br />
+          We collect and process personal data based on the following legal
+          grounds: Your consent. The performance of a contract with you. Our
+          legitimate interests. Compliance with legal obligations.
+          <br />
+          Purposes of Data Processing
+          <br />
+          We use your personal data for the following purposes:
+          <br />
+          Managing your requests and inquiries. Sending you commercial and
+          marketing communications, such as product updates and special offers.
+          Processing your orders, including delivery, invoicing, and customer
+          account creation. Providing after-sale services, including handling
+          returns, exchanges, and repairs. Improving the performance of our
+          website and analyzing visitor trends. Communicating with you about
+          your account, changes to our website, or updates to this Privacy
+          Policy. Preventing payment fraud and ensuring the validity of
+          payments. Defending our interests in case of disputes or legal
+          actions.
         </Text>
       </Wrapper>
+
+      <Text>5. Who Has Access to Your Data?</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          “Karyawan” mengacu pada orang yang telah dipekerjakan di Bisnis.
+          Your personal data is accessed by Repunch staff on a need-to-know
+          basis. Certain third parties may also have access to your data,
+          including: Subcontractors and service providers for after-sale
+          services, logistics, website hosting, payment processing, and other
+          technical and logistical needs. Social media platforms when you use
+          social media buttons on our website. Third-party companies in the
+          event of Repunch&apos;s restructuring, acquisition, or merger.
+          Authorities, courts, or other third parties when required by law,
+          regulations, or judicial decisions to protect and defend our rights.
         </Text>
       </Wrapper>
+
+      <Text>7. Data Retention</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          &quot;Pengguna&quot; mengacu pada setiap orang atau entitas yang
-          menggunakan aspek apa pun dari layanan JOOB.
+          We retain data for as long as necessary for the purpose for which it
+          was collected. The retention period varies depending on the type of
+          data and purpose. In general, data is stored for the duration of our
+          relationship with you and up to three years after its termination.
+          Some data may be archived for legal and administrative reasons for up
+          to 10 years.
         </Text>
       </Wrapper>
-      <Wrapper>
-        <DotWrapper>
-          <Dot />
-        </DotWrapper>
-        <Text>“Layanan” mengacu pada segala aspek layanan JOOB.</Text>
-      </Wrapper>
-      <Text>2 – Ketentuan untuk Pengguna</Text>
+
+      <Text>8. Your Data Protection Rights</Text>
       <Wrapper>
         <DotWrapper>
           <Dot />
         </DotWrapper>
         <Text>
-          Semua Pengguna tidak boleh menggunakan Layanan dengan cara apa pun
-          yang melanggar hak kekayaan intelektual atau hak kepemilikan orang
-          lain di yurisdiksi mana pun.
+          You have the following rights regarding your personal data: Right to
+          access, rectify, and request data portability. Right to erasure
+          (subject to legal obligations). Right to limit data processing. Right
+          to establish instructions for data management after your death. To
+          exercise your rights or for any questions related to your data, please
+          contact us at [Your Email Address].
+        </Text>
+      </Wrapper>
+
+      <Text>9. Data Security</Text>
+
+      <Wrapper>
+        <DotWrapper>
+          <Dot />
+        </DotWrapper>
+        <Text>
+          Repunch employs technical and organizational measures to keep your
+          data secure and confidential. We require our service providers to
+          implement security measures as well. However, please be aware of the
+          inherent risks associated with internet use, and exercise caution when
+          sharing personal information online.
+        </Text>
+      </Wrapper>
+
+      <Text>10. Protection of Minors</Text>
+      <Wrapper>
+        <DotWrapper>
+          <Dot />
+        </DotWrapper>
+        <Text>
+          Our website is not intended for children under the age of [minimum
+          age]. We do not knowingly collect data from minors, except as
+          permitted by law.
+        </Text>
+      </Wrapper>
+
+      <Text>11. Cookies</Text>
+      <Wrapper>
+        <DotWrapper>
+          <Dot />
+        </DotWrapper>
+        <Text>
+          Our website may use cookies to enhance your browsing experience. You
+          can manage your cookie preferences; please refer to our Cookies Policy
+          for more information.
+        </Text>
+      </Wrapper>
+
+      <Text>12. Third-Party Websites</Text>
+      <Wrapper>
+        <DotWrapper>
+          <Dot />
+        </DotWrapper>
+        <Text>
+          Our website may contain links to third-party websites with their own
+          privacy policies. This Privacy Policy does not apply to those
+          websites, so please review their policies to understand how they
+          process your data.
+        </Text>
+      </Wrapper>
+
+      <Text>13. Contact Us</Text>
+      <Wrapper>
+        <DotWrapper>
+          <Dot />
+        </DotWrapper>
+        <Text>
+          If you have any questions, concerns, or complaints related to this
+          Privacy Policy or your personal data, please contact us at [Your Email
+          Address]. If you have a complaint about our data processing practices,
+          you may also contact the appropriate data protection authority in
+          South Korea. Please replace [Your Email Address], [Your Company&apos;s
+          Address in South Korea], [minimum age], and [Date] with your specific
+          information. Additionally, consider consulting with a legal
+          professional to adapt the policy further to your specific business and
+          legal requirements.
         </Text>
       </Wrapper>
     </Container>
