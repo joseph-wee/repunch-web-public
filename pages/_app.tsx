f491fd0588d34995906c945b4d55063a566e468c
joseph-wee <starlightkor@gmail.com>
2024-04-05 13:51:26 +0900

    add&feat: 파비콘에 필요한 파일 혹은 필요할 파일 세팅, 파비콘 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,11 +12,42 @@ export default function App({ Component, pageProps }: AppProps) {
       <GlobalStyle />
       <Head>
         <title>Repunch</title>
-        <meta name="description" content="repunch 웹개발 테스트 사이트" />
+        <meta name="description" content="repunch web" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-startup-image" href="/favicon.ico" />
+
+        {/** 1. 즐겨찾기 아이콘?  2.아이패드 아이콘?*/}
+        {/* <link rel="shortcut icon" href="http://test.repunch.io/favicon.ico" />
+        
+        <link
+          rel="apple-touch-startup-image"
+          href="http://test.repunch.io/favicon.ico"
+        /> */}
+
+        {/** 안드로이드 현재 폰에서는 되는중 탭안되면 고해상도 아이콘 소스코드 추가하면 될 듯? */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+
+        {/** 테마랑 타이틀 관련 컬러인 듯? 나중에 필요하면 참고해서 적용 */}
+        {/* <meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff"></meta> */}
       </Head>
       <Provider store={store}>
         <HeaderBar />
